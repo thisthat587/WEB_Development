e893f9b53085d52ef3c92a86f3d06fb9d1d4b139
Piyush singh <114461687+thisthat587@users.noreply.github.com>
2024-03-05 15:46:44 +0530

    chatApp

---
+++ ChatApp/backend/server.js
@@ -0,0 +1,25 @@
+const app = require('express')();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log("what is socket : ", socket);
+    console.log("socket is active to be connected...");
+
+    socket.on('chat', (payload) => {
+        console.log("what is payload : ", payload);
+        io.emit('chat', payload)
+    });
+});
+
+// app.listen(5000, () => console.log("server is listening at 5000..."))
+
+server.listen(5000, () => {
+    console.log("server is listening at 5000...")
+});
